f4563e76e6550a8c7ea80321c13c7d1f47c8f3fe
Filip Božić <filip.bozic@fer.hr>
2016-12-04 21:00:33 +0100

    Nova aplikacija. Sve bi trebalo radit i radilo je kad sam testirao rukama je bilo oke i u debugeru. Al inace je nes cudno. Kaj sam napravio: samo provjerio da li postoji user s imenom u bazi i poslao odg u obliku stringa nazad true ili false. Ako zelite testirat ovo moze te preko onog plug in-a koji sam vec spominjao za rest

--- IzvorniKod/troskovnik_zivota/src/main/webapp/js/general.js
+++ IzvorniKod/troskovnik_zivota/src/main/webapp/js/general.js
@@ -2,3 +2,7 @@ function showPage() {
 		  document.getElementById("loader").style.display = "none";
 		  document.getElementById("pageContent").style.display = "block";
 }
+
+function changeWindowUrl(url) {
+	window.location.href = url;
+}
